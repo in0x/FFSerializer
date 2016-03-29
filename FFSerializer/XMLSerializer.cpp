8207f5a78bcd3b718b5c9229582ed926ba35e121
Philipp Welsch <pwelsch.mmt-b2014@fh-salzburg.ac.at>
2016-03-29 21:13:36 +0200

    Removed deprecated files

--- FFSerializer/XMLSerializer.cpp
+++
@@ -1,9 +0,0 @@
-#include "stdafx.h"
-#include "XMLSerializer.h"
-
-XMLSerializer::XMLSerializer(const std::string& rootName) : m_currentElement(nullptr)
-{
-	m_document = new tinyxml2::XMLDocument();
-	m_pRoot = m_document->NewElement(rootName.c_str());
-	m_document->InsertFirstChild(m_pRoot);
-};
